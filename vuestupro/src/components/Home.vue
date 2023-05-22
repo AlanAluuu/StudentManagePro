6887ade56e658adf0b59f8b366bf7838a63af589
AlanAluuu <124226943+AlanAluuu@users.noreply.github.com>
2023-05-22 09:37:47 +0800

    Add files via upload

---
+++ vuestupro/src/components/Home.vue
@@ -0,0 +1,79 @@
+<template>
+    <div style="text-align: center;height: 100%;padding: 0px;margin: 0px;">
+
+        <el-descriptions style="font-size: medium" title="个人中心" :column="1" size="70" border>
+            <el-descriptions-item>
+                <template slot="label">
+                    <i class="el-icon-s-custom"></i>
+                    账号
+                </template>
+                {{user.no}}
+            </el-descriptions-item>
+            <el-descriptions-item>
+                <template slot="label">
+                    <i class="el-icon-mobile-phone"></i>
+                    电话
+                </template>
+                {{user.phone}}
+            </el-descriptions-item>
+            <el-descriptions-item>
+                <template slot="label">
+                    <i class="el-icon-location-outline"></i>
+                    性别
+                </template>
+                <el-tag
+                        :type="user.sex === '1' ? 'primary' : 'danger'"
+                        disable-transitions><i :class="user.sex==1?'el-icon-male':'el-icon-female'"></i>{{user.sex==1?"男":"女"}}</el-tag>
+            </el-descriptions-item>
+            <el-descriptions-item>
+                <template slot="label">
+                    <i class="el-icon-tickets"></i>
+                    角色
+                </template>
+                <el-tag
+                        type="success"
+                        disable-transitions>{{user.roleId==0?"超级管理员":(user.roleId==1?"管理员":"用户")}}</el-tag>
+
+            </el-descriptions-item>
+
+        </el-descriptions>
+
+        <DateUtils style="position: fixed;bottom: 0;height: 40px;width: 75%;text-align: center;"></DateUtils>
+    </div>
+</template>
+
+<script>
+    import DateUtils from "./DateUtils";
+    export default {
+        name: "Home",
+        components: {DateUtils},
+        data() {
+
+            return {
+                user:{}
+            }
+        },
+        computed:{
+
+        },
+        methods:{
+            init(){
+                this.user = JSON.parse(sessionStorage.getItem('CurUser'))
+            }
+        },
+        created(){
+            this.init()
+        }
+    }
+</script>
+
+<style scoped>
+    .el-descriptions{
+        width:90%;
+
+        margin: 0 auto;
+        text-align: center;
+    }
+
+
+</style>
---
+++ vuestupro/src/store/index.js
@@ -0,0 +1,51 @@
+import vue from 'vue'
+import Vuex from 'vuex'
+import router, {resetRouter} from "../router";
+import createPersistedState from 'vuex-persistedstate'
+vue.use(Vuex)
+
+function addNewRoute(menuList) {
+    console.log(menuList)
+    let routes = router.options.routes//路由列表
+    console.log(routes)
+    routes.forEach(routeItem=>{
+        if(routeItem.path=="/Index"){
+            menuList.forEach(menu=>{
+                let childRoute =  {
+                    path:'/'+menu.menuclick,
+                    name:menu.menuname,
+                    meta:{
+                        title:menu.menuname
+                    },
+                    component:()=>import('../components/'+menu.menucomponent)
+                }
+                routeItem.children.push(childRoute)
+            })
+        }
+    })
+
+    resetRouter()
+    router.addRoutes(routes)
+}
+
+export default new Vuex.Store({
+    state: {
+        menu: []
+    },
+    mutations: {
+        setMenu(state,menuList) {
+            state.menu = menuList
+
+            addNewRoute(menuList)
+        },
+        setRouter(state,menuList) {
+            addNewRoute(menuList)
+        }
+    },
+    getters: {
+        getMenu(state) {
+            return state.menu
+        }
+    },
+    plugins:[createPersistedState()]
+})
